6b54536cfbd2faf9571d6a8bbab217e32883185e
CHOJUNGHO96 <jo4186@naver.com>
2023-10-30 02:33:43 +0900

    프로그래머스_고득점kit/그리디/2단계_조이스틱

--- 프로그래머스_고득점kit/그리디/2단계_조이스틱.py
+++ 프로그래머스_고득점kit/그리디/2단계_조이스틱.py
@@ -1,90 +1,41 @@
-def solution(단어: str) -> int:
-    import string
-
-    정답 = 0
-    플러스_알파벳 = list(string.ascii_uppercase[1:13])
-    마이너스_알파벳 = sorted(list(string.ascii_uppercase[13:]), reverse=True)
-    인덱스 = 0
-
-    단어 = list(단어)
-    while True:
-
-        # 현위치에서 알파벳 변경
-        if 단어[인덱스] in 플러스_알파벳:
-            정답 += 플러스_알파벳.index(단어[인덱스]) + 1
-            단어[인덱스] = "A"
-        elif 단어[인덱스] in 마이너스_알파벳:
-            정답 += 마이너스_알파벳.index(단어[인덱스]) + 1
-            단어[인덱스] = "A"
-
-        # 모든 알파벳이 A로 되었는지 확인
-        if 단어 == list("A" * len(단어)):
-            return 정답
-
-        # 커서 이동을 위한 변수 설정
-        왼쪽, 오른쪽 = 1, 1
-
-        # 커서를 왼쪽으로 움직이는 경우
-        while 단어[인덱스 - 왼쪽] == "A":
-            왼쪽 += 1
-
-        # 커서를 오른쪽으로 움직이는 경우
-        while 단어[인덱스 + 오른쪽] == "A":
-            오른쪽 += 1
-
-        # 왼쪽과 오른쪽 중 어디로 움직이는 것이 더 효율적인지 판단
-        if 왼쪽 < 오른쪽:
-            정답 += 왼쪽
-            인덱스 -= 왼쪽
-        else:
-            정답 += 오른쪽
-            인덱스 += 오른쪽
-
-
-def solution1(알파벳: str) -> int:
-    # 초기 조작 횟수를 0으로 설정
+def solution(단어):
+    """
+    1. 알파벳 변경한값저장
+    2. 위치 변경
+    """
     정답 = 0
 
-    # 알파벳 변경을 위한 최소 이동 횟수 계산(아스키코드활용)
-    최소알파벳수 = [min(ord(i) - ord("A"), ord("Z") - ord(i) + 1) for i in 알파벳]
+    # 처음 최소이동거리는 단어의 길이 - 1로세팅한다
+    최소이동거리 = len(단어) - 1
+    for 인덱스, 알파벳 in enumerate(단어):
+        # 해당알파벳을 위, 아래로 움직인 경우의 최솟값을 정답에 넣어준다
+        정답 += min(ord(알파벳) - ord("A"), ord("Z") - ord(알파벳) + 1)
 
-    인덱스 = 0
+        # next 세팅은 처음위치에서 알파벳까지의 이동으로 세팅
+        next = 인덱스 + 1
 
-    while True:
-        # 현재 커서 위치에서 알파벳을 목표 알파벳으로 변경
-        정답 += 최소알파벳수[인덱스]
+        # 알파벳 이후의 연속된 A의 값찾기
+        while next < len(단어) and 단어[next] == "A":
+            next += 1
 
-        # 변경이 완료되면 0으로 표시
-        최소알파벳수[인덱스] = 0
+        """        
+        2번 (2 * 인덱스) + len(단어) - next : 오른쪽으로 알파벳만큼갔다가 다시 원복후 왼쪽으로 이동
+        ex : BBBBAABB 인경우 연속된 A전까지 3번을 갔다가 원복하므로 (3*2) 한후 왼쪽으로 이동해야하므로 (len(단어) - next(연속된A까지의 인덱스포함임))
+        
+        3번 인덱스 + 2 * (len(단어) - next) : 왼쪽으로 알파벳만큼 갓다가 원복후 오른쪽으로 이동
+        ex : BBBBAABB 인경우 왼쪽으로 연속된 A까지 왕복한값이므로 (len(단어) - next(연속된A까지의 인덱스포함임))에 오른쪽으로 해당 인덱스까지더한값 
+        """
 
-        # 모든 알파벳이 A로 되었는지 확인
-        if sum(최소알파벳수) == 0:
-            return 정답
-
-        # 커서 이동을 위한 변수 설정
-        왼쪽, 오른쪽 = 1, 1
-
-        # 커서를 왼쪽으로 움직이는 경우
-        while 최소알파벳수[인덱스 - 왼쪽] == 0:
-            왼쪽 += 1
-
-        # 커서를 오른쪽으로 움직이는 경우
-        while 최소알파벳수[인덱스 + 오른쪽] == 0:
-            오른쪽 += 1
-
-        # 왼쪽과 오른쪽 중 어디로 움직이는 것이 더 효율적인지 판단
-        if 왼쪽 < 오른쪽:
-            정답 += 왼쪽
-            인덱스 -= 왼쪽
-        else:
-            정답 += 오른쪽
-            인덱스 += 오른쪽
+        최소이동거리 = min(최소이동거리, (2 * 인덱스) + len(단어) - next, 인덱스 + 2 * (len(단어) - next))
+    return 정답 + 최소이동거리
 
 
 # print(solution("AAAAAA"))  # 0
 # print(solution("AAAACB"))  # 5
+print(solution("BBBBAABB"))  # 11
 # print(solution("AABAAAAAAABBB"))  # 11
-print(solution("CANAAAAANAN"))  # 48
+# print(solution("AABAAAAABBBAAAAAAABA"))  # 11
+# print(solution("CANAAAAANAN"))  # 48
 # print(solution("ABAAAAABAB"))  # 8
 # print(solution("BBBAAB"))  # 9
 # print(solution("CAAAAAA"))  # 2
