2a887d2221e7e77a28e0ffc7848564e9a98ff56c
jhcho <cho418651@ssrinc.co.kr>
2023-11-15 10:39:07 +0900

    프로그래머스_고득점kit/해시/3단계_베스트앨범.py

--- 프로그래머스_고득점kit/해시/3단계_베스트앨범.py
+++ 프로그래머스_고득점kit/해시/3단계_베스트앨범.py
@@ -10,34 +10,48 @@ def solution(노래장르, 재생횟수):
     """
     정답 = []
     임시_dic = {}
+
+    # 인덱스와 재생횟수를 결합하기위한작업
+    # ex : [[0, 500], [1, 600], [2, 150], [3, 800], [4, 2500]]
     재생횟수 = [[인덱스, 값] for 인덱스, 값 in enumerate(재생횟수)]
 
+    # 노래장르에속한 재생횟수에대한 dict 를 만들어준다
+    # ex : {'classic': [[0, 500], [2, 150], [3, 800]], 'pop': [[1, 600], [4, 2500]]}
     for 장르, 횟수 in zip(노래장르, 재생횟수):
         if 장르 not in 임시_dic:
             임시_dic[장르] = []
         임시_dic[장르].append(횟수)
 
+    # 장르와 장르에해당한 재생횟수의 총합을 묶기위한 list
     임시_list = []
+
     for 장르, 횟수 in 임시_dic.items():
+        # 장르에 속한 노래중 재생횟수가 많은순서대로 정렬
+        # ex :{'classic': [0, 500], [2, 150]], [[3, 800]} --> {'classic': [[3, 800], [0, 500], [2, 150]]}
         횟수.sort(key=lambda x: x[1], reverse=True)
 
+        # 장르에속한 재생횟수의 총합을구하여 임시_list에 장르와 총합을 묶어서 append
+        # [['classic', 1450], ['pop', 3100]]
         총합 = sum([i[1] for i in 횟수])
         임시_list.append([장르, 총합])
+
+        # 총합을 다구했으므로 배스트앨범2개까지만 남긴다
+        # ex : {'classic': [[3, 800], [0, 500]]}
         if len(횟수) > 2:
-            횟수.pop()
+            임시_dic[장르] = 횟수[:2]
+
+    # 재생횟수가 가장많은 장르순으로 정렬
+    # ex : [['classic', 1450], ['pop', 3100]] --> [['pop', 3100], ['classic', 1450]]
     임시_list.sort(key=lambda x: x[1], reverse=True)
 
+    # 정답에 정렬된 장르순으로 장르에속한 앨범들을 넣어준다
     for 값 in 임시_list:
         정답 += [i[0] for i in 임시_dic[값[0]]]
 
     return 정답
 
 
-print(
-    solution(
-        ["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]
-    )
-)
+print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
 print(
     solution(
         ["jazz", "rap", "hiphop", "jazz", "jazz", "hiphop"],
