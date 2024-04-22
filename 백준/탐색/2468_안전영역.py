3d50f305915e0ae5f87ae3e0ae38a26b57d3f36b
jhcho <cho418651@ssrinc.co.kr>
2024-04-22 16:57:25 +0900

    백존 2468_안전영역 copy.deepcopy 제거

--- 백준/탐색/2468_안전영역.py
+++ 백준/탐색/2468_안전영역.py
@@ -11,8 +11,8 @@ sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 result = 0
 max_value = 0
-# while True:
 n = int(input())
+
 # 행렬만들기
 graph = []
 for i in range(n):
@@ -21,27 +21,24 @@ for i in range(n):
     if max_value < max(tmp_arr):
         max_value = max(tmp_arr)
 
-import copy
 
 for mv in range(1, max_value + 1):
-    tmp_graph = copy.deepcopy(graph)
+    visited = [[False] * n for _ in range(n)]
 
     def dfs(x, y):
         if x < 0 or x >= n or y < 0 or y >= n:
             return False
-        if tmp_graph[x][y] >= mv:
-            tmp_graph[x][y] = 0
+        if graph[x][y] >= mv and not visited[x][y]:
+            visited[x][y] = True
             dfs(x + 1, y)
             dfs(x - 1, y)
             dfs(x, y + 1)
             dfs(x, y - 1)
-        else:
-            tmp_graph[x][y] = 0
 
     tmp = 0
     for i in range(n):
         for j in range(n):
-            if tmp_graph[j][i] >= mv:
+            if graph[j][i] >= mv and not visited[j][i]:
                 dfs(j, i)
                 tmp += 1
     if tmp > result:
