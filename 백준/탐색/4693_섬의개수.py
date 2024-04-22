928b2d087807b9f6bb7ea3e5ad7adc972525edca
jhcho <cho418651@ssrinc.co.kr>
2024-04-22 10:58:42 +0900

    백준 4693_섬의개수 bfs풀이 추가

--- 백준/탐색/4693_섬의개수.py
+++ 백준/탐색/4693_섬의개수.py
@@ -3,6 +3,7 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+# dfs 풀이법
 while True:
     w, h = map(int, input().split())
 
@@ -39,4 +40,49 @@ while True:
                 dfs(i, j)
                 result += 1
 
+print(result)
+
+
+# bfs 풀이법
+
+from collections import deque
+
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    graph = []
+    for i in range(m):
+        graph.append(list(map(int, input().split())))
+
+    def bfs(x, y):
+        que = deque([(x, y)])
+        while que:
+            move = que.popleft()
+            if move[0] < 0 or move[0] >= n or move[1] < 0 or move[1] >= m:
+                continue
+
+            x = move[0]
+            y = move[1]
+            if graph[y][x] == 1:
+                # 상하 좌우
+                que.append((x + 1, y))
+                que.append((x - 1, y))
+                que.append((x, y + 1))
+                que.append((x, y - 1))
+
+                # 대각선
+                que.append((x + 1, y + 1))
+                que.append((x - 1, y + 1))
+                que.append((x + 1, y - 1))
+                que.append((x - 1, y - 1))
+
+                graph[move[1]][move[0]] = 0
+
+    result = 0
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 1:
+                bfs(j, i)
+                result += 1
     print(result)
